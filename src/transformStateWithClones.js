6ea0e6091f9bc09170813da202ce6fc6df408b68
Viktoria <vasylchenkoviktoria@gmail.com>
2025-11-06 15:32:26 +0200

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  let currentState = { ...state };
+  const history = [];
 
+  for (const action of actions) {
+    let nextState = { ...currentState };
+
+    switch (action.type) {
+      case 'clear':
+        nextState = {};
+        history.push({ ...nextState });
+        currentState = nextState;
+        break;
+
+      case 'addProperties':
+        Object.assign(nextState, action.extraData);
+        history.push({ ...nextState });
+        currentState = nextState;
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete nextState[key];
+        }
+        history.push({ ...nextState });
+        currentState = nextState;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return history;
+}
 module.exports = transformStateWithClones;
